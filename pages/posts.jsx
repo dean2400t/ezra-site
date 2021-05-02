26c24c09740a0bf97dd9ef25566269cf126be2d8
dean2400t <dean2400t@gmail.com>
2021-05-02 10:19:02 +0300

    start

--- pages/posts.jsx
+++ pages/posts.jsx
@@ -1,25 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Ezra Site</title>
-        <meta name="description" content="אתר לצעירי חולון" />
+        <meta name="description" content="מודעות צעירי חולון" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <div>
         <h1 className={styles.title}>
-          האתר
+          מודעות
         </h1>
-        <Link href="/posts">
-          <a>פרסומים</a>
-        </Link>
         </div>
       </main>
     </div>
