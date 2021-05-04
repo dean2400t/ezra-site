73a08798beac65eb1184ed74ca5da630dd5b57e9
dean2400t <dean2400t@gmail.com>
2021-05-04 11:52:54 +0300

    implemented Static Generation with revalidate

--- pages/scholarships.jsx
+++ pages/scholarships.jsx
@@ -1,5 +1,27 @@
-export default function WorkShops() {
-    return (<div>
-        <h1>מלגות</h1>
-    </div>)
+import {PrismaClient} from '@prisma/client';
+const prisma = new PrismaClient();
+
+export async function getStaticProps() {
+    const scholarships = await prisma.scholarship.findMany();
+    return {
+      props: {scholarships},
+      revalidate: 60
+    }
+  }
+
+function WorkShops({scholarships}) {
+    return (
+        <ul>
+            {scholarships.map((scholarship) => (
+                <li>
+                    <br/><br/>
+                    {scholarship.title} <br/>
+                    {scholarship.amount} <br/>
+                    {scholarship.content}
+                </li>
+            ))}
+        </ul>
+    )
 }
+
+export default WorkShops;
